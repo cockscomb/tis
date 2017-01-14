63691bea5e768724a46a686896be6c0926f39049
cockscomb <cockscomb@hatena.ne.jp>
2017-01-14 23:21:11 +0900

    Add list and select subcommand

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,1 +1,19 @@
-print("Hello, world!")
+import TextInputSources
+import Commander
+
+let main = Group {
+    $0.command("list", description: "List enabled input sources") {
+        for inputSource in TextInputSources.find() {
+            print(inputSource.id)
+        }
+    }
+
+    $0.command("select", description: "Select an input source") { (id: String) in
+        guard let inputSource = TextInputSources.find(filtering: [.id: id]).first else {
+            return
+        }
+        try TextInputSources.select(inputSource)
+    }
+}
+
+main.run()
